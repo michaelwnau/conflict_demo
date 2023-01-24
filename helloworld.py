68ed1855394d202b0f21b635ee54a528adef409c
Michael Nau <michaelwnau@gmail.com>
2023-01-24 14:52:34 -0600

    commit

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,9 @@
-<< << << < HEAD
-
-
-def hello_world(city, state):
+def hello_world(city):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-== == == =
-
-
-def hello_world(city, state):
-    print(f'Hello World {city}, {state}!')
-
-
-def main():
-    hello_world('NYC', 'New York')
-
-
->>>>>> > 49742adddf5ebdc905c97636f92a42fb04bad01a
 
 
 main()
